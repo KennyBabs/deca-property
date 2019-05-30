967b7ee2791a0a13c81999c10df51b76cec4a18b
Babalola Kehinde <babalolakehinde.p@gmail.com>
2019-05-30 04:20:36 +0100

    add form submit function to populate the database with user details

--- src/index.js
+++ src/index.js
@@ -1,43 +1,70 @@
-$('.form').find('input').on('keyup blur focus', function (e) {
+$(document).ready(function(){
+
+    $('.form').find('input').on('keyup blur focus', function (e) {
   
-    var $this = $(this),
-        label = $this.prev('label');
-  
-        if (e.type === 'keyup') {
-              if ($this.val() === '') {
-            label.removeClass('active highlight');
-          } else {
-            label.addClass('active highlight');
-          }
-      } else if (e.type === 'blur') {
-          if( $this.val() === '' ) {
-              label.removeClass('active highlight'); 
+        var $this = $(this),
+            label = $this.prev('label');
+      
+            if (e.type === 'keyup') {
+                  if ($this.val() === '') {
+                label.removeClass('active highlight');
               } else {
-              label.removeClass('highlight');   
-              }   
-      } else if (e.type === 'focus') {
-        
-        if( $this.val() === '' ) {
-              label.removeClass('highlight'); 
-              } 
-        else if( $this.val() !== '' ) {
-              label.addClass('highlight');
+                label.addClass('active highlight');
               }
-      }
-  
-  });
-  
-  $('.tab a').on('click', function (e) {
-    
-    e.preventDefault();
-    
-    $(this).parent().addClass('active');
-    $(this).parent().siblings().removeClass('active');
-    
-    target = $(this).attr('href');
-  
-    $('.tab-content > div').not(target).hide();
-    
-    $(target).fadeIn(600);
-    
-  });
+          } else if (e.type === 'blur') {
+              if( $this.val() === '' ) {
+                  label.removeClass('active highlight'); 
+                  } else {
+                  label.removeClass('highlight');   
+                  }   
+          } else if (e.type === 'focus') {
+            
+            if( $this.val() === '' ) {
+                  label.removeClass('highlight'); 
+                  } 
+            else if( $this.val() !== '' ) {
+                  label.addClass('highlight');
+                  }
+          }
+      
+      });
+      
+      $('.tab a').on('click', function (e) {
+        
+        e.preventDefault();
+        
+        $(this).parent().addClass('active');
+        $(this).parent().siblings().removeClass('active');
+        
+        target = $(this).attr('href');
+      
+        $('.tab-content > div').not(target).hide();
+        
+        $(target).fadeIn(600);
+        
+      });
+
+      $('form').submit(function(e){
+
+        e.preventDefault();
+        let formData = {
+            "first_name"  : $('input[name=first_name]').val(),
+            "last_name"   : $('input[name=last_name]').val(),
+            "email"       : $('input[name=email]').val(),
+            "password"    : $('input[name=password]').val()
+        };
+
+        $.ajax({
+            type         :  'POST',
+            url          :  'http://localhost:3000/users',
+            contentType  :   "application/json",
+            data         :   formData,
+            dataType     :   'json',
+            encode       :   true
+        })
+        .done(function(data){
+            console.log(data);
+        });
+
+      });
+});

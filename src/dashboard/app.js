204fb01fd62b4bb936a0854765ccd40c6e65489b
Babalola Kehinde <babalolakehinde.p@gmail.com>
2019-05-31 05:14:08 +0100

    add function to fetch all property

---
+++ src/dashboard/app.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+  $(".view-prop-btn").click(viewAllProperties);
+
+  function viewAllProperties(){
+  
+    $.ajax({
+
+      type    :  'GET',
+      url     :   'http://localhost:3000/property',
+      success :   function(data){
+
+        $(".view-prop").empty();
+        for(var i = 0; i < data.length; i++){
+          console.log("data",data);
+          $(".view-prop").append("<div class='card'><img src="+data[i].photos +" alt='property image' class='image'><div class='container'><h4><b>"+data[i].type+"</b></h4><p>Location: "+data[i].address+"</p><p>"+data[i].description+"</p><p>Price: "+data[i].price+"</p><button id="+data[i].id+" class='deletePost'>Delete</button><button id="+data[i].id+" class='getOneProp'>Details</button>");
+          $(".view-prop").append("");
+          $(".view-prop").append("");
+          $("section").hide();
+          $(".frm-content").hide();
+        }
+      },
+        error:function(){
+          console.log("error");
+        }
+    });
+  }
+    
+})

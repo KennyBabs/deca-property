75f8bb716a36912968370aa2b4e24603c1657459
Babalola Kehinde <babalolakehinde.p@gmail.com>
2019-05-31 04:06:30 +0100

    create user login authorization

--- src/index.js
+++ src/index.js
@@ -87,4 +87,28 @@ $(document).ready(function(){
       
     }
 })
+
+// User login
+
+$('.login').submit(function(e){
+    e.preventDefault();
+    let email = $('input[name=emal]').val();
+    let password = $('input[name=passwrd]').val();
+
+    $.ajax({
+        method: 'GET',
+        url: 'http://localhost:3000/users',
+        dataType: "json"
+    }).done(function(data){
+        let user = data.find(
+            user => user.email === email && user.password === password);
+            if(user){
+                alert("Welcome back!");
+
+            }else{
+                alert("You have to sign-up")
+            }
+    })
+    })
+
 })
